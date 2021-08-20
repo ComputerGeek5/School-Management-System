2ead05365a73def8edeaeb66bc7558ab2ef235f1
Primus <primus@Primuss-MacBook-Pro.local>
2021-08-20 16:46:34 +0200

    Added create form

---
+++ resources/views/admins/create.blade.php
@@ -0,0 +1,21 @@
+@extends("layouts.app")
+
+@section("content")
+    <h1 class="pb-5">Add User</h1>
+
+    {!! Form::open(["action" => "App\Http\Controllers\AdminsController@store", "method" => "POST", "enctype" => "multipart/form-data"]) !!}
+    <div class="form-group">
+        {{ Form::label("name", "Name") }}
+        {{ Form::text("name", "", ["class" => "form-control", "placeholder" => "Name"]) }}
+    </div>
+    <div class="form-group">
+        {{ Form::label("email", "Email") }}
+        {{ Form::email("email", "", ["class" => "form-control", "placeholder" => "Email"]) }}
+    </div>
+    <div class="form-group">
+        {{ Form::label("password", "Password") }}
+        {{ Form::password("password", ["class" => "form-control", "placeholder" => "Password"]) }}
+    </div>
+    {{ Form::submit('Create', ["class" => "btn btn-lg btn-success"]) }}
+    {!! Form::close() !!}
+@endsection
