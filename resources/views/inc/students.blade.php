3db431f7cf8b47fe5974e1fe669a276f89afc2a9
Primus <primus@Primuss-MacBook-Pro.local>
2021-08-26 11:49:36 +0200

    Added icons to navbar

--- resources/views/inc/students.blade.php
+++ resources/views/inc/students.blade.php
@@ -1,9 +1,9 @@
 <li class="nav-item">
-    <a class="nav-link" href="/students/{{ Auth::user()->id }}">Your Profile</a>
+    <a class="nav-link" href="/students/{{ Auth::user()->id }}"><i class="fas fa-user-circle"></i>    Profile</a>
 </li>
 <li class="nav-item">
-    <a class="nav-link" href="/students/take">Take Courses</a>
+    <a class="nav-link" href="/students/take"><i class="fas fa-book-open"></i>  Take Courses</a>
 </li>
 <li class="nav-item">
-    <a class="nav-link" href="/students/selected">Selected Courses</a>
+    <a class="nav-link" href="/students/selected"><i class="fas fa-edit"></i>   Selected Courses</a>
 </li>
--- resources/views/inc/teachers.blade.php
+++ resources/views/inc/teachers.blade.php
@@ -1,9 +1,9 @@
 <li class="nav-item">
-    <a class="nav-link" href="/teachers/{{ Auth::user()->id }}">Your Profile</a>
+    <a class="nav-link" href="/teachers/{{ Auth::user()->id }}"><i class="fas fa-user-circle"></i>    Profile</a>
 </li>
 <li class="nav-item">
-    <a class="nav-link" href="/teachers/courses">Your Courses</a>
+    <a class="nav-link" href="/teachers/courses"><i class="fas fa-book-open"></i>  Your Courses</a>
 </li>
 <li class="nav-item">
-    <a class="nav-link" href="/teachers/courses/create">Add New Course</a>
+    <a class="nav-link" href="/teachers/courses/create"><i class="fas fa-plus-square"></i>   Add New Course</a>
 </li>
